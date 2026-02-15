049383e3ce8b953ed507d1ed22a8465899f31f81
Karthick Raja Ramasa <karthickramasamy1194@gmail.com>
2026-02-15 18:12:08 +0530

    updated ecs and github actions

---
+++ terraform/ecs.tf
@@ -0,0 +1,95 @@
+# Find the default VPC in your account
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Find all subnets inside that default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_iam_role" "ecs_exec_role" {
+  name = "${var.project_name}-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+# Attach the standard AWS policy for ECS Task Execution
+resource "aws_iam_role_policy_attachment" "ecs_exec_role_policy" {
+  role       = aws_iam_role.ecs_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_security_group" "api_sg" {
+  name        = "${var.project_name}-api-sg"
+  description = "Allow inbound traffic to Task Tracker API"
+  vpc_id      = data.aws_vpc.default.idyes
+
+  ingress {
+    from_port   = 8000 # Change this to match your app port (FastAPI usually 8000)
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# The Cluster
+resource "aws_ecs_cluster" "main" {
+  name = "${var.project_name}-cluster"
+}
+
+# The Task Definition (Blueprint)
+resource "aws_ecs_task_definition" "api_task" {
+  family                   = "task-tracker-api"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256" # 0.25 vCPU
+  memory                   = "512" # 0.5 GB
+  execution_role_arn       = aws_iam_role.ecs_exec_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "api-container"
+      image     = aws_ecr_repository.api_repo.repository_url # Link to your ecr.tf resource
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+        }
+      ]
+    }
+  ])
+}
+
+# The Service (Manager)
+resource "aws_ecs_service" "api_service" {
+  name            = "${var.project_name}-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.api_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    assign_public_ip = true
+    security_groups  = [aws_security_group.api_sg.id]
+  }
+}
